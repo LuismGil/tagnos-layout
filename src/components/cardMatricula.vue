e23ac259941042483eaee2b64864f950e8fc1c05
Maria Lares <maria.lares.26@gmail.com>
2023-12-18 17:05:10 -0400

    add listagem de matriculas

---
+++ src/components/cardMatricula.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="container-status-card">
+    <div class="container-status-card__bar"/>
+    <div class="container-status-card__header">
+      <div class="container-status-card__header__left">
+        <q-icon name="far fa-eye"/>
+        <span class="container-status-card__header__left__name">MAICON JEFERSON SIMON  RODRIGUES DOS SANTOS</span> |
+        <span> <b>MATRÍCULA</b> 555555</span>
+      </div>
+      <q-icon
+        :name="show ? 'far fa-chevron-up': 'fas fa-chevron-down'"
+        @click="show=!show"
+      />
+    </div>
+    <div
+      v-if="show"
+      class="container-status-card__body"
+    >
+      <div class="container-status-card__body__primaria">
+        <span class="container-status-card__body__primaria__titulo">CHAMADA</span>
+        <span class="container-status-card__body__primaria__conteudo">111</span>
+        <span class="container-status-card__body__primaria__titulo">Nível</span>
+        <span class="container-status-card__body__primaria__conteudo">1º Ano</span>
+        <span class="container-status-card__body__primaria__titulo">Turno</span>
+        <span class="container-status-card__body__primaria__conteudo">MATUTINO</span>
+        <span class="container-status-card__body__primaria__titulo">Turma</span>
+        <span class="container-status-card__body__primaria__conteudo">TURMA(DANILO)</span>
+      </div>
+      <div class="container-status-card__body__secundaria">
+        <q-icon name="fas fa-edit"/>
+        <q-icon name="fas fa-file"/>
+        <q-icon name="fas fa-print"/>
+        <q-icon name="far fa-times"/>
+        <q-icon name="fas fa-trash-alt"
+          color="red"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CardMatricula',
+  props: {
+    matriculas: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+  created () {
+  }
+})
+</script>
+
+<style
+  scoped
+  lang="scss"
+>
+
+.container-status-card {
+  border-radius: 8px;
+  border: 1px solid #D4D4D4;
+  background: #FFF;
+  color: #68717A;
+  position: relative;
+
+  &__bar {
+    background-color: blue;
+    border-radius: 8px;
+    height: 70px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 6px;
+  }
+
+  &__header {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    padding: 25px;
+
+    &__left {
+      .q-icon {
+        margin: 0 12px 0 0;
+      }
+
+      &__name {
+        font-weight: 600;
+      }
+    }
+
+    .q-icon {
+      cursor: pointer;
+      font-weight: 600;
+    }
+  }
+
+  &__body {
+    background-color: #D1E5F880;
+    border-top: 1px solid #D4D4D4;
+    padding: 25px;
+
+    &__primaria {
+      &__titulo {
+        font-weight: 600;
+        margin: 0 4px 0 0;
+      }
+      &__conteudo {
+        margin: 0 15px 0 0;
+      }
+    }
+    &__secundaria {
+      font-size: 20px;
+      margin: 25px 0 0 0;
+      .q-icon {
+        font-weight: 400;
+        margin: 0 24px 0 0;
+      }
+    }
+  }
+
+}
+
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,26 +19,6 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -46,71 +26,33 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
-
   components: {
-    EssentialLink
   },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
+  data () {
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      toggleLeftDrawer: false
     }
+  },
+  create: {
   }
 })
 </script>
+
+<style
+  scoped
+  lang="scss"
+>
+  .q-page-container {
+    background-color: $bgSecondary;
+    min-height: 100vh;
+    /* padding: 100px 5% 90px 5%;
+
+    @media (min-width: map-get($mediaQueries, 'md')) {
+      padding: 100px 0 90px 370px ;
+    } */
+  }
+</style>
