7f224ce462ec55412870ea88301fffaf313f0cc0
LuismGil <luism6977@gmail.com>
2023-12-18 16:44:52 -0300

    import fonts

---
+++ src/layouts/MainHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>holi</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainHeader',
+  components: {},
+  data () {
+    return {
+    }
+  }
+})
+</script>
+<style
+  scoped
+  lang='scss'
+>
+</style>
