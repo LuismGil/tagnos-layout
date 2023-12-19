8584558bed70044b3576f83d11a204c899843e59
Bruno Pires <brunosouza2049@gmail.com>
2023-12-19 17:15:07 -0400

    update-MainMenu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,12 +1,18 @@
 <template>
   <q-drawer
+    v-model="drawer"
     show-if-above
-    bordered
-    style="background-color: #337DCE;"
-    class="text-white"
+    :mini="miniState"
+    @mouseover="miniState = false"
+    @mouseout="miniState = true"
+    mini-to-overlay
+    :width="300"
+    :breakpoint="500"
+    style="background-color: #184372"
+    class="menu text-white"
   >
     <div class="flex justify-center mt-[29px] mb-[33px]">
-      <img src="../assets/img/Logo-municipio.svg" alt="">
+      <img src="../assets/img/Logo-municipio.svg" alt="" />
     </div>
     <q-list>
       <q-expansion-item
@@ -16,19 +22,20 @@
         :label="item.label"
       >
         <q-list
-          class="q-pl-lg m-[16px] rounded-[10px]"
-          style="background-color: #245890;"
+          class="m-[16px] rounded-[10px]"
+          style="background-color: #245890"
         >
           <q-item
+            class="rounded-[10px]"
             v-for="(itemSubMenu, indexItemSubMenu) in item.itensSubMenu"
             :key="indexItemSubMenu"
             :to="{ name: `${itemSubMenu.to}` }"
             active-class="q-item-no-link-highlighting"
+            @click="selectItem(item, indexItemSubMenu)"
+            :class="{ selected: itemSubMenu.selected }"
           >
             <q-item-section avatar>
-              <q-icon
-                :name="itemSubMenu.icon"
-              />
+              <q-icon :name="itemSubMenu.icon" />
             </q-item-section>
             <q-item-section>
               <q-item-label>{{ itemSubMenu.label }}</q-item-label>
@@ -36,20 +43,23 @@
           </q-item>
         </q-list>
       </q-expansion-item>
-      <q-btn
-        class="w-[150px] h-[44px] text-red"
-        style="position:absolute; bottom: 6%; left: 50%; transform: translateX(-50%); background-color: #FFDDE3; text-transform: capitalize;"
-      >
-        <q-icon
-          name="exit_to_app"
-        /> &nbsp;
-        <b>
-          Sair
-        </b>
-      </q-btn>
-      <div class="flex" style="position: absolute; bottom: 20px; left: 50%; transform: translateX(-50%);">
-        <img src="../assets/icons/icon-versao.svg" alt="">&nbsp; Versão > 1.38.0
-      </div>
+
+      <q-item-section class="menu_footer flex column items-center w-full">
+        <div
+          class="mb-[20px]"
+          style="border-top: 2px solid white; width: 80%"
+        ></div>
+        <q-btn class="btn_Sair w-[150px]" @click="sair">
+          <q-icon name="exit_to_app" />
+          &nbsp; <b>Sair</b></q-btn
+        >
+        <div class="mt-[20px]">
+          <div class="info">
+            <q-icon name="fa-regular fa-code-branch" />
+            Versão > 1.38.0
+          </div>
+        </div>
+      </q-item-section>
     </q-list>
   </q-drawer>
 </template>
@@ -58,14 +68,15 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'MainMenu',
-  components: {
-  },
+  components: {},
   data () {
     return {
+      drawer: false,
+      miniState: true,
       itensMenu: [
         {
           label: 'Matrículas',
-          icon: 'fa-solid fa-file',
+          icon: 'fa-regular fa-file',
           itensSubMenu: [
             {
               to: 'matriculas',
@@ -73,52 +84,69 @@ export default defineComponent({
               icon: 'fa-regular fa-file'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Rematrículas',
+              icon: 'fa-regular fa-file-plus'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Pré-Matrículas',
+              icon: 'fa-regular fa-user-check'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Uniformes',
+              icon: 'fa-regular fa-tag'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Ocorrências',
+              icon: 'fa-regular fa-clipboard'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Histórico Escolar',
+              icon: 'fa-regular fa-box-archive'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Novo Histórico Escolar',
+              icon: 'fa-regular fa-list'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Transferências',
+              icon: 'fa-regular fa-repeat'
             },
             {
-              to: 'matriculas',
-              label: 'Matrículas',
-              icon: 'fa-regular fa-file'
+              to: '',
+              label: 'Fotos por Matrícula',
+              icon: 'fa-regular fa-camera'
             }
           ]
         },
         {
-          label: 'Matrículas',
-          icon: 'fa-solid fa-file'
+          label: 'Manutenções',
+          icon: 'fa-light fa-wrench'
+        },
+        {
+          label: 'Lançamentos',
+          icon: 'fa-regular fa-folder'
+        },
+        {
+          label: 'Consultas',
+          icon: 'fa-solid fa-search'
         }
       ]
+    }
+  },
+  methods: {
+    selectItem (item, index) {
+      item.itensSubMenu.forEach((subItem) => {
+        subItem.selected = false
+      })
+
+      item.itensSubMenu[index].selected = true
     }
   }
 })
@@ -130,5 +158,24 @@ export default defineComponent({
 >
 .q-router-link--exact-active {
   font-weight: 600;
+}
+.menu {
+  position: relative;
+}
+.menu_footer {
+  position: absolute;
+  bottom: 30px;
+}
+.btn_Sair {
+  background-color: #ffdde3;
+  color: #e6492d;
+  text-transform: capitalize;
+}
+.selected {
+  background-color: #123760;
+}
+.q-router-link--exact-active:hover {
+  background-color: #1C4B7D;
+  border-left: 5px solid #123760;
 }
 </style>
