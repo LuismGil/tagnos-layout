928d3a5fe89a2512be86b19f12cb3d7f3ac134f5
LuismGil <luism6977@gmail.com>
2023-12-21 15:42:41 -0300

    adicionando tabs fixas

--- src/pages/Matricula.vue
+++ src/pages/Matricula.vue
@@ -17,7 +17,24 @@
     </div>
     <q-separator />
     <div class="matricula__tabs">
-
+      <q-tabs
+        v-model="tab"
+        inline-label
+        outside-arrows
+        mobile-arrows
+        active-color="white"
+        active-bg-color="primary"
+        indicator-color="primary"
+        class="matricula__tabs__wp"
+      >
+        <q-tab
+          v-for="(tab, tabIndex) in listaTabs"
+          :key="tabIndex"
+          :name="tab.name"
+          :icon="tab.icon"
+          :label="tab.label"
+        />
+      </q-tabs>
     </div>
   </div>
 </template>
@@ -37,7 +54,50 @@ export default defineComponent({
         { descricao: 'Transferências' },
         { descricao: 'Matrículas', active: true },
         { descricao: 'Uniformes' }
-      ]
+      ],
+      listaTabs: [
+        {
+          name: 'Aluno',
+          icon: 'fa-regular fa-user',
+          label: 'Aluno'
+        },
+        {
+          name: 'Matrícula',
+          icon: 'fa-regular fa-file-lines',
+          label: 'Matrícula'
+        },
+        {
+          name: 'Responsável',
+          icon: 'fa-light fa-user-group',
+          label: 'Responsável'
+        },
+        {
+          name: 'Documentações',
+          icon: 'fa-regular fa-folder',
+          label: 'Documentações'
+        },
+        {
+          name: 'Ocorrências',
+          icon: 'fa-regular fa-message',
+          label: 'Ocorrências'
+        },
+        {
+          name: 'Uniformes',
+          icon: 'fa-regular fa-tag',
+          label: 'Uniformes'
+        },
+        {
+          name: 'Dados Clínicos',
+          icon: 'fa-regular fa-wave-pulse',
+          label: 'Dados Clínicos'
+        },
+        {
+          name: 'Observações',
+          icon: 'fa-regular fa-clipboard',
+          label: 'Observações'
+        }
+      ],
+      tab: 'Aluno'
     }
   }
 })
@@ -47,19 +107,21 @@ export default defineComponent({
   scoped
   lang="scss"
 >
+@import 'src/css/base/_mixins.scss';
 .matricula {
+  padding: 25px 60px 15px;
+
   &__title {
     color: #3E464E;
     font-size: 32px;
     font-weight: 600;
-    margin: 25px 60px 15px;
   }
 
   &__telas {
     display: flex;
     flex-direction: row;
     gap: 33px;
-    margin: 0 60px 22px;
+    margin: 15px 0 22px;
 
     &__wp {
       display: flex;
@@ -74,15 +136,23 @@ export default defineComponent({
 
     }
   }
+
+  &__tabs {
+    margin: 35px 0;
+
+    &__wp {
+      @include boxWhite;
+      border-radius: 4px;
+      color: #68717A !important;
+      font-size: 16px;
+      font-weight: 500;
+    }
+
+  }
 }
 
 .active {
   color: #123760;
-}
-
-.q-separator {
-  height: 1px;
-  margin: 0 60px;
 }
 
 </style>
